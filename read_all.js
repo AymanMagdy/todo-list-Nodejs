86b6f916012bc8955a886254a25f9ca976e302a9
AymanMagdy <magdia883@gmail.com>
2020-04-09 23:45:56 +0200

    adding comment to the file

--- read_all.js
+++ read_all.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// Read all data from the file.
 function read_all(){
     const data = fs.readFileSync('todo_data.txt', 'UTF-8');
     const lines = data.split(/\r?\n/);
