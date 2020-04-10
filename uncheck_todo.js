9eba9362781c6e9e008fe8b91ceeb1fed2a95a77
AymanMagdy <magdia883@gmail.com>
2020-04-10 23:22:55 +0200

    finished the unchecked todo

---
+++ uncheck_todo.js
@@ -0,0 +1,31 @@
+const read_all_caller = require('./read_all')
+const append_to_file_caller = require('./append_to_file')
+
+function uncheck_todo(todo_id){
+    all_todo_data = read_all_caller.read_all()
+    new_items = []
+    // Get the needed value.
+    for(i=0; i < all_todo_data.length-1; i++){
+        line_json = JSON.parse(all_todo_data[i]);
+        if(line_json['id'] == todo_id){
+            if(line_json['checked'] == false){
+                console.log("Already unchecked todo item.");
+                new_items[i] = all_todo_data[i]
+            }
+            else if(line_json['checked'] == true){
+                line_json['checked'] = false
+                new_line_json = JSON.stringify(line_json)
+                new_items.push(new_line_json)
+            }
+        } else if(line_json['id'] != todo_id){
+            new_items.push(all_todo_data[i])
+        }
+    }
+
+    // call append function and pass the new items.
+    append_to_file_caller.append_new_data(new_items)
+}
+
+module.exports = {
+    uncheck_todo
+}
