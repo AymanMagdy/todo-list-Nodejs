49debe6747376f77fffbfa080ba6a59b302f1e9e
AymanMagdy <magdia883@gmail.com>
2020-04-10 22:58:57 +0200

    check a todo function

---
+++ check_todo.js
@@ -0,0 +1,25 @@
+const read_all_caller = require('./read_all')
+const append_to_file_caller = require('./append_to_file')
+
+function check_todo(todo_id){
+    all_todo_data = read_all_caller.read_all()
+    new_items = []
+    // Get the needed value.
+    for(i=0; i < all_todo_data.length-1; i++){
+        line_json = JSON.parse(all_todo_data[i]);
+        if(line_json['id'] == todo_id){
+            line_json['checked'] = true
+            new_line_json = JSON.stringify(line_json)
+            new_items.push(new_line_json)
+        } else if(line_json['id'] != todo_id){
+            new_items[i] = all_todo_data[i]
+        }
+    }
+
+    // call append function and pass the new items.
+    append_to_file_caller.append_new_data(new_items)
+}
+
+module.exports = {
+    check_todo
+}
