415860423d7ba44497e6ec0a140e89d8584cdd84
AymanMagdy <magdia883@gmail.com>
2020-04-10 13:49:25 +0200

    done the checked items

---
+++ checked_items.js
@@ -0,0 +1,18 @@
+const read_all_caller = require('./read_all')
+
+function get_checked_items(){
+    checked_items = []
+    all_todo_data = read_all_caller.read_all()
+    for(i=0; i < all_todo_data.length-1; i++){
+        line_json = JSON.parse(all_todo_data[i]);
+        if(line_json['checked'] == true){
+            checked_items.push(line_json)
+        }
+    }
+
+    return checked_items;
+}
+
+module.exports = {
+    get_checked_items
+}
