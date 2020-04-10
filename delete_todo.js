fd4b65d3da4f5c40303058fe4bf3d14a1242bd0a
AymanMagdy <magdia883@gmail.com>
2020-04-10 22:46:32 +0200

    delete method edit

--- delete_todo.js
+++ delete_todo.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const read_all_caller = require('./read_all')
+const append_to_file_caller = require('./append_to_file')
 
 function delete_todo(todo_id){
     all_todo_data = read_all_caller.read_all()
@@ -12,24 +12,8 @@ function delete_todo(todo_id){
         }
     }
 
-    // delete the file
-    fs.unlink('todo_data.txt', function (err) {
-        if (err) throw err;
-    }); 
-
-    // Creat a new file with the same name
-    fs.open('todo_data.txt','a',function (err) {
-        if (err) throw err;
-    })
-
-    // Add the correct data to the file.
-    for (i=0; i < new_items.length; i++){
-        if(new_items[i] != null){
-            fs.appendFile('todo_data.txt', new_items[i] + "\n", function (err) {
-                if (err) throw err;
-            });
-        }
-    }
+    // call append function and pass the new items.
+    append_to_file_caller.append_new_data(new_items)
 }
 
 module.exports = {
